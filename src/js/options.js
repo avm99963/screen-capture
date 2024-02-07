3cad59dd83dd1d31e1ff1aad1940294292af63d9
Adrià Vilanova Martínez <me@avm99963.com>
2024-02-07 19:34:38 +0100

    refactor: remove unused code
    Change-Id: Ie9653b87e955979dfe21412b643725bae274b069

--- src/js/options.js
+++ src/js/options.js
@@ -2,8 +2,6 @@
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
-var bg = chrome.extension.getBackgroundPage();
-
 function $(id) {
   return document.getElementById(id);
 }
